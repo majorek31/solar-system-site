7df4f75f83fa81648a4ba2401cc3dd3c741849fb
majorek31 <m4y0r.offical@gmail.com>
2023-05-14 01:46:16 +0200

    use rewrites

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
+const rewrite = require('./middlewares/rewriteUrl');
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+app.use(rewrite('/', '/index.html'));
+app.use(rewrite('/login', '/login.html'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
