5611e2b0b69e1041a886abadc83dd526312f93fb
majorek31 <m4y0r.offical@gmail.com>
2023-05-14 10:36:41 +0200

    Create post add

--- src/api/post/index.js
+++ src/api/post/index.js
@@ -3,10 +3,26 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const protectedEndpoint = require('../../middlewares/protectedEndpoint');
+const jwt = require('jsonwebtoken');
 
 router.get('/', async (req, res) => {
     try {
-        let posts = await prisma.post.findMany();
+        let posts = await prisma.post.findMany({
+            select: {
+                id: true,
+                title: true,
+                author_id: true,
+                timeCreated: true,
+                content: false,
+                description: true,
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
+                },
+            },
+        });
         return res.status(200).json({
             status: 'success',
             data: {
@@ -15,6 +31,7 @@ router.get('/', async (req, res) => {
             }
         });
     } catch (err) {
+        if (err && process.env.NODE_ENV !== 'production') console.error(err);
         return res.status(500).json({
             status: 'internal error',
             data: {
@@ -24,8 +41,40 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', protectedEndpoint, async (req, res) => {
-    res.json({noice: true});
+router.post('/', protectedEndpoint(), async (req, res) => {
+    if (!req.body.title || !req.body.content || !req.body.description) {
+        return res.status(400).json({
+            status: 'invalid request',
+            data: {
+                message: 'title and content must be provided!',
+            }
+        });
+    }
+    try {
+        let token = await jwt.decode(req.cookies.jwt);
+        let post = await prisma.post.create({
+            data: {
+                title: req.body.title,
+                content: req.body.content,
+                author_id: token.id,
+                description: req.body.description
+            }
+        });
+        return res.status(200).json({
+            status: 'success',
+            data: {
+                post
+            }
+        });
+    } catch (err) {
+        if (err && process.env.NODE_ENV !== 'production') console.error(err);
+        return res.status(500).json({
+            status: 'internal error',
+            data: {
+                message: 'error with database',
+            }
+        });
+    }
 });
 
 module.exports = router;
