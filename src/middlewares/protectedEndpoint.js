2f1424834f202292aee9b2debce57c72bc914d20
majorek31 <m4y0r.offical@gmail.com>
2023-05-14 10:35:40 +0200

    Add requiredRole as option

--- src/middlewares/protectedEndpoint.js
+++ src/middlewares/protectedEndpoint.js
@@ -4,16 +4,33 @@ const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-module.exports = async (req, res, next) => {
-    try {
-        let token = await jwt.verify(req.cookies.jwt, process.env.JWT_SECRET);
-    } catch (err) {
-        return res.status(403).json({
-            status: 'authorization failed',
-            data: {
-                message: 'invalid token',
+module.exports = (options) => {
+    return async (req, res, next) => {
+        try {
+            let token = await jwt.verify(req.cookies.jwt, process.env.JWT_SECRET);
+            if (options?.requiredRole) {
+                let user = await prisma.user.findFirst({
+                    where: {
+                        id: token.id,
+                    }
+                });
+                if (options?.requiredRole !== user.role) {
+                    return res.status(403).json({
+                        status: 'no access',
+                        data: {
+                            message: 'Insufficient permission',
+                        }
+                    });
+                }
             }
-        });
+        } catch (err) {
+            return res.status(403).json({
+                status: 'authorization failed',
+                data: {
+                    message: 'invalid token',
+                }
+            });
+        }
+        next();
     }
-    next();
 }
