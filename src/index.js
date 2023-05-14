3973ea05cdcb97e1db921158209ab6bc6ceed709
majorek31 <m4y0r.offical@gmail.com>
2023-05-14 02:19:13 +0200

    Add middlewares

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
 const rewrite = require('./middlewares/rewriteUrl');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(rewrite('/', '/index.html'));
 app.use(rewrite('/login', '/login.html'));
